94f7473eb8e8c25100ebb6dbbe6209138768e73d
Jonathan Weston <jonathan.weston.2@gmail.com>
2025-04-06 14:35:38 +0100

    fix delpoyment

--- epaper/update-screen.sh
+++ epaper/update-screen.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 LOGFILE="script_log.log"
-source ~/pimoroni/bin/activate
+echo "Starting servers..." | tee -a $LOGFILE
 
+# Start Node.js server
+node ~/pi/scraping/server.js &> $LOGFILE &
+NODE_PID=sudo lsof -ti tcp:5000
+echo "Node server started with PID: $NODE_PID" | tee -a $LOGFILE
+
+cd ~/pi/reactapp/build
+# Start http-server
+npx http-server &> $LOGFILE &
+HTTP_SERVER_PID=sudo lsof -ti tcp:8080
+echo "http-server started with PID: $HTTP_SERVER_PID" | tee -a $LOGFILE
+
+source ~/pimoroni/bin/activate
 echo "Starting first script..." | tee -a $LOGFILE
 
 # Run the first Python script and wait for completion
@@ -14,3 +26,8 @@ echo "First script completed. Starting second script..." | tee -a $LOGFILE
 python3 display-image.py >> $LOGFILE 2>&1
 
 echo "Second script completed." | tee -a $LOGFILE
+echo "Stopping servers..." | tee -a $LOGFILE
+
+# Kill both processes
+kill $NODE_PID $HTTP_SERVER_PID
+echo "Servers stopped." | tee -a $LOGFILE
