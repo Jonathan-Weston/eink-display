04ca1923f59807ce3131a6d4bf7b4794e587a244
Jonathan Weston <jonathan.weston.2@gmail.com>
2025-04-08 21:38:37 +0100

    remove saturation

--- epaper/display-image.py
+++ epaper/display-image.py
@@ -4,14 +4,12 @@ from inky.auto import auto
 
 inky = auto()
 
-SATURATION = 0.5
-
 PHOTO_PATH = 'react_app_screenshot.png'
 BORDER_COLOR = inky.WHITE
 RESAMPLING = Image.BICUBIC
 
 image = Image.open(PHOTO_PATH)
 resizedimage = ImageOps.pad(image, inky.resolution, RESAMPLING, BORDER_COLOR)
-inky.set_image(resizedimage, saturation=SATURATION)
+inky.set_image(resizedimage)
 inky.set_border(BORDER_COLOR)
 inky.show()
