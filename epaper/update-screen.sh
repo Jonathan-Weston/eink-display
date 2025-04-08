ca9390e1ce40add585fb62704c78f12288c6c1f0
Jonathan Weston <jonathan.weston.2@gmail.com>
2025-04-08 21:11:31 +0100

    update deploy script

--- epaper/update-screen.sh
+++ epaper/update-screen.sh
@@ -12,8 +12,7 @@ node ~/pi/scraping/server.js |
     do echo "[$(date '+%Y-%m-%d %H:%M:%S')] $line"; 
     done >> $LOGFILE &
 
-NODE_PID=$(sudo lsof -ti tcp:5000)
-echo "Node server started with PID: $NODE_PID" | tee /dev/fd/3
+echo "Node server started..." | tee /dev/fd/3
 
 cd ~/pi/reactapp/build
 # Start http-server
@@ -37,12 +36,14 @@ echo "Updating screen..." | tee /dev/fd/3
 python3 display-image.py | tee /dev/fd/3
 
 echo "Stopping servers..." | tee /dev/fd/3
-
+NODE_PID=$(sudo lsof -ti tcp:5000)
 # Kill both processes
 kill $NODE_PID | tee /dev/fd/3
-NODE_PID_2=$(sudo lsof -ti tcp:5000)
-echo "PID For Port 5000 searched for result = $NODE_PID_2" | tee /dev/fd/3
+echo "PID For Port 5000 searched for result = $(sudo lsof -ti tcp:5000)" | tee /dev/fd/3
+
+HTTP_SERVER_PID=$(sudo lsof -ti tcp:8080)
 kill $HTTP_SERVER_PID | tee /dev/fd/3
-HTTP_SERVER_PID_2=$(sudo lsof -ti tcp:8080)
-echo "PID For Port 8080 searched for result = $HTTP_SERVER_PID_2" | tee /dev/fd/3
+echo "PID For Port 8080 searched for result = $(sudo lsof -ti tcp:8080)" | tee /dev/fd/3
 echo "Servers stopped." | tee /dev/fd/3
+
+exit 0;
