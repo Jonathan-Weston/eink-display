45e5ed53e5ae8052f961edabdf738aa2a72b0a5b
Jonathan Weston <jonathan.weston.2@gmail.com>
2025-04-11 10:26:57 +0100

    Minor CSS changes

--- reactapp/src/components/DailyAndWeeklyWeather/ForecastItem/ForecastItem.tsx
+++ reactapp/src/components/DailyAndWeeklyWeather/ForecastItem/ForecastItem.tsx
@@ -21,7 +21,7 @@ export const ForecastItem = ({img, day, maxTempValue, minTempValue, percipitatio
             <img className={styles.forecastItemIcon}
             src={img} alt=''></img>
         </div>
-        <div id ="forecast-5days-item-0-time" className="forecastItemTime">{day}</div>
+        <div id ="forecast-5days-item-0-time" className={styles.forecastItemTime}>{day}</div>
         <ForecastItemValue value={percipitationValue} unit={"%"}></ForecastItemValue>
     </div>
 
